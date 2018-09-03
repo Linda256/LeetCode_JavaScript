4ef2cfce9ecc8cee96a285690e505ea2cc02a6ed
Linda Wang <lindawang256@gmail.com>
2018-09-03 13:37:20 -0700

    Add math method for 7 Reverse Integer

--- 7_ReverseInteger.js
+++ 7_ReverseInteger.js
@@ -44,6 +44,36 @@ var reverse = function(x) {
     return temp<Math.pow(2,31)-1 && temp>Math.pow(-2,31) ? temp : 0;
 };
 
-console.log(reverse(-13456));
+var reverse2 = function(x) {
+  /*
+  if |x| between 0 and 9 return x
+  if x<0 x=|x| negative = true
+  let result = 0;
+  let remain = x;
+  while (remain>10)
+      get each digit d by mod x by 10;
+      remain = Math.floor(result/10)
+      result=result*10+d;
+  if negative = true return -result;
+  return result
+  */
+  if (Math.abs(x)>=0 & Math.abs(x)<10) return x;
+  let negative = false;
+  if (x<0){
+    negative = true;
+    x=-x;
+  }
+  let result = 0;
+  let remain = x;
+  while (remain>0){
+    let d = remain % 10;
+    remain= Math.floor(remain/10);
+    result=result*10+d;
+  }
+  let temp = negative===true ? -result : result;
+  return temp<Math.pow(2,31)-1 && temp>Math.pow(-2,31) ? temp : 0;
+};
+
+console.log(reverse2(-134569999999999));
 
 
