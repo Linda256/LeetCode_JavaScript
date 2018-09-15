a3cec9e80c1976bf6d4ffff4fb29ff1fab771c27
Linda Wang <lindawang256@gmail.com>
2018-09-15 07:54:21 -0700

    163 Add a more concise solution

--- 163_MissingRanges.js
+++ 163_MissingRanges.js
@@ -63,3 +63,30 @@ var findMissingRanges = function(nums, lower, upper) {
         }
     return result;
 };
+
+//method 2
+var findMissingRanges = function(nums, lower, upper) {
+
+  let ranges = [];
+  nums.unshift(lower);
+  nums.push(upper);
+
+  for (let i = 0; i < nums.length-1; i++) {
+
+    let l = nums[i]+1;
+
+    let r = nums[i+1]-1;
+
+    if (i == 0) l--;
+    if (i+1 == nums.length-1) r++;
+
+
+    if (l > r) continue;
+
+    if (l == r) ranges.push(`${l}`);
+
+    else ranges.push(`${l}->${r}`);
+  }
+
+  return ranges;
+};
