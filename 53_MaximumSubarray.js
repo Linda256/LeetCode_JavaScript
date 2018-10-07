482d44378b04c2e16e4c05c7d2fa81abe5104129
Linda Wang <lindawang256@gmail.com>
2018-10-07 08:28:22 -0700

    53 Max SubArray naive method

---
+++ 53_MaximumSubarray.js
@@ -0,0 +1,38 @@
+/*
+53.Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+/*
+@param {number[]} nums
+@return {number}
+*/
+//method 1: naive method.
+// time complexity O(n*n)
+var maxSubArray=function(nums){
+  let maxSum=nums[0];
+  for(let i=0;i<nums.length-1;i++){
+    let currSum=nums[i];
+    for(let j=i+1;j<nums.length;j++){
+      currSum=currSum+nums[j];
+      maxSum=Math.max(maxSum,Math.max(currSum,nums[j]))
+    }
+  }
+  return maxSum;
+}
+
+
+
+//let nums=[-2,1,-3,4,-1,2,1,-5,4];
+//let nums=[-2,1];
+let nums=[10,-2,-3,-2];
+console.log(maxSubArray(nums));
