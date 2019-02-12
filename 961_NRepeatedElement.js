8467606ba62018f027294e5de3635770940181db
Linda Wang <lindawang256@gmail.com>
2019-02-12 15:40:16 -0800

    kth nonrepeated character

--- 961_NRepeatedElement.js
+++ 961_NRepeatedElement.js
@@ -20,3 +20,28 @@ var repeatedNTimes = function(A) {
 
 let input=[5,1,5,2,5,3,5,4];
 console.log(repeatedNTimes(input));
+
+/*2nd non-repeat character*/
+
+const secondNonRepeat=function(str,k){
+    let visited={};
+    let nonRepeat=0;
+    for(let i=0;i<str.length;i++){
+        visited[str[i]]===undefined?visited[str[i]]=1:visited[str[i]]+=1;
+    }
+
+    for(let i=0;i<str.length;i++){
+        if(visited[str[i]]===1&&nonRepeat<k){
+            nonRepeat++;
+        }
+        else if(visited[str[i]]===1&&nonRepeat===k){
+            return str[i];
+        }
+    }
+    return -1;
+}
+
+let str = "AmericanExpress";
+let k=1;
+let res=secondNonRepeat(str,k);
+console.log('nonRepeat',res);
