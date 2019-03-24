3305b8bf353dd08bdd336c737eb7a0b4ea037f68
Linda Wang <lindawang256@gmail.com>
2019-03-24 12:24:48 -0700

    1023 binary string add toString(2) method

--- 1023_BinaryStringWithSubStrings.js
+++ 1023_BinaryStringWithSubStrings.js
@@ -2,6 +2,21 @@
 1023 Binary string with substrings representing 1 to N
 
 Given a binary string S (a string consisting only of '0' and '1's) and a positive integer N, return true if and only if for every integer X from 1 to N, the binary representation of X is a substring of S.
+
+Example 1:
+
+Input: S = "0110", N = 3
+Output: true
+Example 2:
+
+Input: S = "0110", N = 4
+Output: false
+
+
+Note:
+
+1 <= S.length <= 1000
+1 <= N <= 10^9
 */
 
 /**
@@ -29,6 +44,15 @@ function toBinary(n){
   return res.reverse().join('');
 }
 
+
+//Methd2 using toString(2) method to conver a number to a binary string
+
+var queryString2=function(S,N){
+  for (let i=1;i<=N;i++){
+    if(S.indexOf(i.toString(2))===-1) return false;
+  }
+  return true;
+}
 //let n=3;
 // let n=8;
 // console.log(toBinary(n));
