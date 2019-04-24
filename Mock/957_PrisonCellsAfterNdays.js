8e9d7326e51261be3999df311af5857ff76d20cd
Linda Wang <lindawang256@gmail.com>
2019-04-24 12:06:04 -0700

    Prison Cells After Ndays

---
+++ Mock/957_PrisonCellsAfterNdays.js
@@ -0,0 +1,69 @@
+
+/*There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+Otherwise, it becomes vacant.
+(Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)
+
+
+
+*/
+/*
+Example 1:
+
+Input: cells = [0,1,0,1,1,0,0,1], N = 7
+Output: [0,0,1,1,0,0,0,0]
+Explanation: 
+The following table summarizes the state of the prison on each day:
+Day 0: [0, 1, 0, 1, 1, 0, 0, 1]
+Day 1: [0, 1, 1, 0, 0, 0, 0, 0]
+Day 2: [0, 0, 0, 0, 1, 1, 1, 0]
+Day 3: [0, 1, 1, 0, 0, 1, 0, 0]
+Day 4: [0, 0, 0, 0, 0, 1, 0, 0]
+Day 5: [0, 1, 1, 1, 0, 1, 0, 0]
+Day 6: [0, 0, 1, 0, 1, 1, 0, 0]
+Day 7: [0, 0, 1, 1, 0, 0, 0, 0]
+
+Example 2:
+
+Input: cells = [1,0,0,1,0,0,1,0], N = 1000000000
+Output: [0,0,1,1,1,1,1,0]
+ 
+
+Note:
+
+cells.length == 8
+cells[i] is in {0, 1}
+1 <= N <= 10^9
+
+*/
+
+var prisonAfterNDays = function(cells, N) {
+    //the pattern repeated every 14 days
+    N=N%14;
+    if(N===0) N=14;
+    while(N>0){
+      let newCells=[];
+        //loop throught cells(start at 1)
+      for(let i=1;i<cells.length-1;i++){
+        newCells[0]=0;
+        newCells[cells.length-1]=0;
+        if (cells[i-1]===cells[i+1] ){
+          newCells[i]=1;
+        } else {
+          newCells[i]=0  
+        } 
+      }
+      cells=newCells;
+      N--; 
+      
+    }
+    return cells;
+    
+};
