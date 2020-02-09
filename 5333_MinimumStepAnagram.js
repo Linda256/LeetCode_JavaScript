2f2f16a07544c530bfa9322b63e9011f98cf6fd7
Linda Wang <lindawang256@gmail.com>
2020-02-08 20:28:23 -0800

    Add description

--- 5333_MinimumStepAnagram.js
+++ 5333_MinimumStepAnagram.js
@@ -1,20 +1,34 @@
-// let minSteps = function(s, t) {
-//     //edge case: space 
+/*
+Given two equal-size strings s and t. In one step you can choose any character of t and replace it with another character.
 
-//     s.split('').sort((a,b)=>a-b);
-//     t.split('').sort((a,b)=>a-b);
-//     if(s===t){
-//         return 0;
-//     } 
-//     let count=0
-//     for(let i=0;i<s.length;i++){
-//         if(s[i]!==t[i]){
-//             count++;
-//         }
-//     }
-//     return count;
+Return the minimum number of steps to make t an anagram of s.
 
-// };
+An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+Example 1:
+
+Input: s = "bab", t = "aba"
+Output: 1
+Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+Example 2:
+
+Input: s = "leetcode", t = "practice"
+Output: 5
+Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+Example 3:
+
+Input: s = "anagram", t = "mangaar"
+Output: 0
+Explanation: "anagram" and "mangaar" are anagrams. 
+Example 4:
+
+Input: s = "xxyyzz", t = "xxyyzz"
+Output: 0
+Example 5:
+
+Input: s = "friend", t = "family"
+Output: 4
+*/
 
 let minSteps = function(s, t) {
     let count=0
