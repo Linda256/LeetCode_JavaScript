1beda0aa78fb86105a00a237f6e348ab2b1f51a2
Linda Wang <lindawang256@gmail.com>
2020-07-04 14:40:26 -0700

    Add MaxAreaOfIsland

---
+++ 659_MaxAreaOfIsland.js
@@ -0,0 +1,75 @@
+/**
+ 695 Max Area of Island
+ Given a non-empty 2D array grid of 0's and 1's, 
+ an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+
+Find the maximum area of an island in the given 2D array. 
+(If there is no island, the maximum area is 0.)
+
+Example 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+Given the above grid, return 6. Note the answer is not 11, 
+because the island must be connected 4-directionally.
+Example 2:
+
+[[0,0,0,0,0,0,0,0]]
+Given the above grid, return 0.
+Note: The length of each dimension in the given grid does not exceed 50.
+ * 
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function (grid) {
+  let maxCount = 0;
+  let count = 0;
+
+  const dfs = (row, col, grid) => {
+    if (row < 0 || col < 0 || row > grid.length - 1 || col > grid[0].length - 1) {
+      return;
+    }
+    if (grid[row][col] === 0) {
+      return;
+    }
+    if (grid[row][col] === 1) {
+      grid[row][col] = 0;
+      count++;
+    }
+
+    dfs(row + 1, col, grid);
+    dfs(row - 1, col, grid);
+    dfs(row, col + 1, grid);
+    dfs(row, col - 1, grid);
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 1) {
+        dfs(i, j, grid);
+        maxCount = count > maxCount ? count : maxCount;
+      }
+      count = 0;
+    }
+  }
+  return maxCount;
+};
+
+let grid = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+];
+
+console.log(maxAreaOfIsland(grid));
